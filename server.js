bc21dfe0f29b5e112bcf33f9a0b989e83eac905b
vivekkandregula <vivekkandregula20@gmail.com>
2026-04-09 11:24:20 -0700

    Fixed port binding for Render

--- server.js
+++ server.js
@@ -92,6 +92,6 @@ app.get('/api/teams', (req, res) => {
 
 // Dynamic player endpoints removed for Stateless Architecture
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
